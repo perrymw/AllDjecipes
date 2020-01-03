52af35749af05c1deaaa4104a3c65f806e77debf
Matthew Perry <perry357@outlook.com>
2020-01-03 09:48:33 -0500

    started skeleton for users model and mostly flushed out.  Models to be imporoved upon

---
+++ alldjecipes/recipes/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+
+
+# if settings.DEBUG:
+#         urlpatterns += static(settings.MEDIA_URL,
+#                               document_root=settings.MEDIA_ROOT)
